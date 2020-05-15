1121fbadd0794aa0af9ed2c228a820ea60c272e6
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-05-15 03:13:31 -0300

    create table

---
+++ src/create.js
@@ -0,0 +1,10 @@
+const models = require('./models')
+
+async function create(){
+      
+  await models.sequelize.sync({force: true})
+  await models.sequelize.close()
+
+  console.log("Banco sincronizado");
+}
+create()
